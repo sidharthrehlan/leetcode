c22e1babb3903f7ef92df18f357968f726346ff0
sidharthrehlan <sidharth.rehlan@olx.com>
2022-05-26 10:40:24 +0530

    lru solution

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-function LRUCache(size) {
-  let cacheList = new Map();
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.cacheList = new Map();
+  this.size = capacity;
+};
 
-  this.get = function (key) {
-    let value = -1;
-    if (cacheList.has(key)) {
-      value = cacheList.get(key);
-      cacheList.delete(key);
-      cacheList.set(key, value);
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  let value = -1;
+  if (this.cacheList.has(key)) {
+    value = this.cacheList.get(key);
+    this.cacheList.delete(key);
+    this.cacheList.set(key, value);
+  }
+
+  console.log(this.cacheList);
+  return value;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  //key does not exist in the list
+
+  if (this.get(key) !== -1 && this.cacheList.size < this.size) {
+    this.cacheList.set(key, value);
+  } else if (this.get(key) === -1 && this.cacheList.size >= this.size) {
+    for (let item of this.cacheList) {
+      console.log('item---', item[0]);
+      this.cacheList.delete(item[0]);
+      break;
     }
+    this.cacheList.set(key, value);
+  } else {
+    this.cacheList.set(key, value);
+  }
 
-    console.log(cacheList);
-    return value;
-  };
+  console.log(this.cacheList);
+};
 
-  this.put = function (key, value) {
-    //key does not exist in the list
-
-    if (this.get(key) !== -1 && cacheList.size < size) {
-      cacheList.set(key, value);
-    } else if (this.get(key) === -1 && cacheList.size >= size) {
-      for (let item of cacheList) {
-        console.log('item---', item[0]);
-        cacheList.delete(item[0]);
-        break;
-      }
-      cacheList.set(key, value);
-    } else {
-      cacheList.set(key, value);
-    }
-
-    console.log(cacheList);
-  };
-}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
 
 let cache = new LRUCache(2);
 cache.put(1, 'sid');
