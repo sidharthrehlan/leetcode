64be875c540c783acdbed27685a4f6c5664f7d44
sidharthrehlan <sidharth.rehlan@olx.com>
2022-05-26 10:29:40 +0530

    LRU

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-const arr = [-1, 2, 0, 3, 4, 1, -8, 16]; //10
-const findMaxSum = (arr) => {
-  let maxSum = 0;
-  let length = arr.length;
+function LRUCache(size) {
+  let cacheList = new Map();
 
-  for (let i = 0; i < length; i++) {
-    let sum = 0;
-    for (let j = i; j < length; j++) {
-      sum = sum + arr[j];
-
-      if (sum > maxSum) {
-        maxSum = sum;
-      }
+  this.get = function (key) {
+    let value = -1;
+    if (cacheList.has(key)) {
+      value = cacheList.get(key);
+      cacheList.delete(key);
+      cacheList.set(key, value);
     }
-  }
-  return maxSum;
-};
 
-console.log(findMaxSum(arr));
+    console.log(cacheList);
+    return value;
+  };
+
+  this.put = function (key, value) {
+    //key does not exist in the list
+
+    if (this.get(key) !== -1 && cacheList.size < size) {
+      cacheList.set(key, value);
+    } else if (this.get(key) === -1 && cacheList.size >= size) {
+      for (let item of cacheList) {
+        console.log('item---', item[0]);
+        cacheList.delete(item[0]);
+        break;
+      }
+      cacheList.set(key, value);
+    } else {
+      cacheList.set(key, value);
+    }
+
+    console.log(cacheList);
+  };
+}
+
+let cache = new LRUCache(2);
+cache.put(1, 'sid');
+cache.put(2, 'kama');
+cache.put(3, 'komal');
