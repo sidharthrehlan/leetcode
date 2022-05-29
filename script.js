76d4286f55a38c278bb42d4235ae7334b99b5f92
sidharthrehlan <sidharth.rehlan@olx.com>
2022-05-29 17:22:30 +0530

    island problem

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-// publisher
-// Subscriber
-// unsubscribe
-// Some place to store callbacks that are registered from subscribers.
-
-function pubSub() {
-  // object which will track of all events and subscription
-  const subscribers = {};
-
-  // Publisher:
-  function publish(eventName, data) {
-    // return if event is not subscribed
-    if (!Array.isArray(subscribers[eventName])) {
-      return;
-    }
-
-    // Whenever you publish any event, it will trigger callback for all stored event in subscriber object
-    subscribers[eventName].forEach((callback) => {
-      callback(data);
-    });
+function findSurroundingIslands(x, y, grid) {
+  if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) {
+    return;
   }
 
-  // Subscriber
-  function subscribe(eventName, callback) {
-    if (!Array.isArray(subscribers[eventName])) {
-      subscribers[eventName] = [];
+  if (grid[(x, y)] === '-1' || grid[(x, y)] === '0') {
+    grid[x][y] = '-1';
+    return;
+  } else {
+    if (grid[x][y] === '1') {
+      //find surrounding land
+      grid[x][y] = '-1';
+      findSurroundingIslands(x - 1, y, grid);
+      findSurroundingIslands(x + 1, y, grid);
+      findSurroundingIslands(x, y - 1, grid);
+      findSurroundingIslands(x, y + 1, grid);
     }
-    //on subscribe we will we will push callback to subscribers[eventName] array
-    subscribers[eventName].push(callback);
-    const index = subscribers[eventName].length - 1;
-
-    // subscribed callbacks to be removed when they are no longer necessary.
-    return {
-      unsubscribe() {
-        subscribers[eventName].splice(index, 1);
-      },
-    };
   }
-
-  return {
-    publish,
-    subscribe,
-  };
 }
+
+var numIslands = function (grid) {
+  let islandCount = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        findSurroundingIslands(i, j, grid); //
+        islandCount = islandCount + 1;
+        grid[i][j] = '-1';
+      }
+    }
+  }
+  return islandCount;
+};
+
+let inputGrid = [
+  ['1', '1', '0', '0', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '1', '0', '0'],
+  ['0', '0', '0', '1', '1'],
+];
+
+console.log(numIslands(inputGrid));
