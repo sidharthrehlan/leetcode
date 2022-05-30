c5d5629f8423d4844b7e93e887fd2e38ce36d8d2
sidharthrehlan <sidharth.rehlan@olx.com>
2022-05-30 11:52:12 +0530

    reverse

--- script.js
+++ script.js
@@ -1,67 +1,17 @@
-var tictactoe = function (moves) {
-  let winArrayA = new Array(8).fill(0);
-  let winArrayB = new Array(8).fill(0);
+var reverse = (str) => {
+  let length = str1.length;
 
-  // [rowwin, columnwin, primary diagonal, secondary diagonal]
-
-  let winArray;
-
-  for (let i = 0; i < moves.length; i++) {
-    let x = moves[i][0];
-    let y = moves[i][1];
-
-    if (i % 2 === 0) {
-      player = 'A';
-      winArray = winArrayA;
-    } else {
-      player = 'B';
-      winArray = winArrayB;
-    }
-
-    // add row values
-    winArray[x] = winArray[x] + 1;
-    if (isWin(winArray[x])) {
-      return player;
-    }
-    //add column values
-    winArray[3 + y] = winArray[3 + y] + 1;
-    if (isWin(winArray[3 + y])) {
-      return player;
-    }
-    //add primary diagonal values
-    if (x === y) {
-      winArray[6] = winArray[6] + 1;
-      if (isWin(winArray[6])) {
-        return player;
-      }
-    }
-    //add secondary diagonal values
-    if (y === 2 - x) {
-      winArray[7] = winArray[7] + 1;
-      if (isWin(winArray[7])) {
-        return player;
-      }
-    }
-  }
-  if (moves.length === 9) {
-    return 'Draw';
-  }
-  if (moves.length < 9) {
-    return 'Pending';
-  }
+  reverseStr(str1, 0, length - 1);
 };
 
-function isWin(count) {
-  if (count === 3) {
-    return true;
+function reverseStr(str, l, r) {
+  if (l > r || l === r) {
+    return;
   }
-}
 
-let moves = [
-  [0, 0],
-  [1, 1],
-  [0, 1],
-  [0, 2],
-  [1, 0],
-  [2, 0],
-];
+  let temp = str[l];
+  str[l] = str[r];
+  str[r] = temp;
+
+  reverseStr(str, l + 1, r - 1);
+}
